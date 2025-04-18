2e304afc4c9b1e9369eba3c5d4db7e0ab0e9b0eb
FaMuLan <fa_mulan@163.com>
2025-04-18 20:35:35 +0800

    黑白两个阵营的显示

--- src/piece_instance.gd
+++ src/piece_instance.gd
@@ -4,6 +4,7 @@ class_name PieceInstance
 var chessboard:Chessboard = null
 var chessboard_name:String = ""
 var position_name:String = ""
+var group:int = 0
 
 func _ready() -> void:
 	chessboard = Chess.get_current_chessboard()
@@ -14,6 +15,13 @@ func _ready() -> void:
 	else:
 		position_name = chessboard.get_position_name(position)
 		position = chessboard.convert_name_to_position(position_name)
+		
+	var material:StandardMaterial3D = StandardMaterial3D.new()
+	if group == 1:
+		material.albedo_color = Color(0, 0, 0, 1)
+	else:
+		material.albedo_color = Color(1, 1, 1, 1)
+	$piece.set_surface_override_material(0, material)
 
 func move(_position_name:String) -> void:
 	position_name = _position_name
