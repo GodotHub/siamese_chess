4178c94225166bd4b7c6ed6b8df608710c302020
FaMuLan <fa_mulan@163.com>
2025-04-19 16:38:31 +0800

    点击和拖动立即走棋

--- src/chessboard.gd
+++ src/chessboard.gd
@@ -1,17 +1,12 @@
 extends Node3D
 class_name Chessboard
 
-var position_name_from:String = ""
-var position_name_to:String = ""
 var chessboard_name:String = "test"
 
 func _ready() -> void:
 	Chess.set_current_chessboard(self)	# 调试用：直接运行棋盘场景时设置当前位置
-	$player.connect("start_drawing_navi", start_drawing_navi)
-	$player.connect("drawing_navi", drawing_navi)
-	$player.connect("end_drawing_navi", end_drawing_navi)
-	$player.connect("cancel_drawing_navi", cancel_drawing_navi)
-	$player.connect("confirm", confirm)
+	$player.connect("tap_position", tap_position)
+	$player.connect("confirm_navi", confirm_navi)
 	var pieces:Dictionary = Chess.get_pieces_in_chessboard(chessboard_name)
 	for key:String in pieces:
 		var piece:PieceInstance = Chess.get_piece_instance(chessboard_name, key)
@@ -24,41 +19,18 @@ func get_position_name(_position:Vector3) -> String:
 func convert_name_to_position(_name:String) -> Vector3:
 	return get_node(_name).position
 
-func start_drawing_navi(position_name:String) -> void:
-	$canvas.clear_lines()
+func tap_position(position_name:String) -> void:
+	$canvas.clear_points()
 	if !Chess.has_piece(chessboard_name, position_name):
 		return
-	position_name_from = position_name
-	$canvas.start_drawing($canvas.convert_name_to_position(position_name))
 	for i:int in range(8):
 		for j:int in range(8):
 			var _position_name_to = "%c%d" % [i + 97, j + 1]
 			if Chess.is_navi_valid(chessboard_name, position_name, _position_name_to):
 				$canvas.draw_point($canvas.convert_name_to_position(_position_name_to))
 
-func drawing_navi(position_name:String) -> void:
-	if !position_name_from:
-		return
-	if !Chess.is_navi_valid(chessboard_name, position_name_from, position_name):
-		return
-	$canvas.drawing_straight($canvas.convert_name_to_position(position_name))
-	position_name_to = position_name
-
-func end_drawing_navi() -> void:
-	$canvas.end_drawing()
-	$canvas.clear_points()
-	confirm()
-
-func cancel_drawing_navi() -> void:
-	$canvas.cancel_drawing()
-
-func check_piece() -> void:
-	pass
-
-func confirm() -> void:
-	if !position_name_from || !position_name_to:
+func confirm_navi(position_name_from:String, position_name_to:String) -> void:
+	if !position_name_from || !position_name_to || !Chess.is_navi_valid(chessboard_name, position_name_from, position_name_to):
 		return
 	Chess.execute_navi(chessboard_name, position_name_from, position_name_to)
-	position_name_from = ""
-	position_name_to = ""
-	$canvas.clear_lines()
+	$canvas.clear_points()
--- src/player.gd
+++ src/player.gd
@@ -1,13 +1,12 @@
 extends Node3D
 
-signal start_drawing_navi(position_name:String)
-signal drawing_navi(position_name:String)
-signal end_drawing_navi()
-signal cancel_drawing_navi()
-
-signal confirm()
+signal tap_position(position_name:String)
+signal confirm_navi(position_name_from:String, position_name_to:String)
 
 @onready var ray_cast:RayCast3D = $ray_cast
+
+var inspect_position_name:String = ""
+var mouse_moved:bool = false
 
 func _ready() -> void:
 	pass
@@ -16,42 +15,49 @@ func _unhandled_input(event:InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed && event.button_index == MOUSE_BUTTON_LEFT:
 			var position_name:String = click(event.position)
-			if position_name:
-				start_drawing_navi.emit(position_name)
-		elif !event.pressed && event.button_index == MOUSE_BUTTON_LEFT:
-			end_drawing_navi.emit()
+			if !inspect_position_name:
+				tap_position.emit(position_name)
+				inspect_position_name = position_name
+			else:
+				confirm_navi.emit(inspect_position_name, position_name)
+				inspect_position_name = ""
+			mouse_moved = false
+		elif !event.pressed && mouse_moved && event.button_index == MOUSE_BUTTON_LEFT:
+			var position_name:String = click(event.position)
+			confirm_navi.emit(inspect_position_name, position_name)
+			inspect_position_name = ""
 	if event is InputEventMouseMotion:
-		if !event.button_mask & MOUSE_BUTTON_MASK_LEFT:
-			return
-		var position_name:String = click(event.position)
-		if !position_name:
-			#cancel_drawing_navi.emit()
-			return
-		drawing_navi.emit(position_name) 
+		mouse_moved = true
 
-	if event is InputEventSingleScreenTouch:
-		var position_name:String = click(event.position)
-		if position_name && event.pressed:
-			start_drawing_navi.emit(position_name)
-		else:
-			end_drawing_navi.emit()
-	if event is InputEventSingleScreenTap:
-		click(event.position)
+	#if event is InputEventSingleScreenTouch:
+	#	var position_name:String = click(event.position)
+	#	if position_name && event.pressed:
+	#		start_drawing_navi.emit(position_name)
+	#	else:
+	#		end_drawing_navi.emit()
+	#if event is InputEventSingleScreenTap:
+	#	var position_name:String = click(event.position)
+	#	if !inspect_position_name:
+	#		tap_position.emit(position_name)
+	#		inspect_position_name = position_name
+	#	else:
+	#		confirm_navi.emit(inspect_position_name, position_name)
+	#		inspect_position_name = ""
 		# 查询position_name位置下的状态
-	if event is InputEventSingleScreenDrag:
-		var position_name:String = click(event.position)
-		if position_name:
-		# 在position_name位置上绘制
-			drawing_navi.emit(position_name)
+	#if event is InputEventSingleScreenDrag:
+	#	var position_name:String = click(event.position)
+	#	if position_name:
+	#	# 在position_name位置上绘制
+	#		drawing_navi.emit(position_name)
 	if event is InputEventMultiScreenDrag:
-		cancel_drawing_navi.emit()
+		#cancel_drawing_navi.emit()
 		rotation.y -= event.relative.x / 300
 		$head.rotation.x -= event.relative.y / 300
 	if event is InputEventScreenPinch:
-		cancel_drawing_navi.emit()
+		#cancel_drawing_navi.emit()
 		$head/camera.position.z -= event.relative / 200
-	if event is InputEventKey && event.pressed && event.keycode == KEY_SPACE:
-		confirm.emit()
+	#if event is InputEventKey && event.pressed && event.keycode == KEY_SPACE:
+	#	confirm.emit()
 
 func click(screen_position:Vector2) -> String:
 	var from:Vector3 = $head/camera.project_ray_origin(screen_position)
