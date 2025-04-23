5fbaf650be5d4baeedc3328447a50a5be8ef6e90
FaMuLan <fa_mulan@163.com>
2025-04-23 13:57:10 +0800

    设计模式改造得差不多了

--- src/chess.gd
+++ src/chess.gd
@@ -42,14 +42,168 @@ class PieceKing extends PieceInterface:
 			answer.push_back(position_name_to)
 		return answer
 
+class PieceQueen extends PieceInterface:
+	static func create_instance(position_name:String, group:int) -> PieceInstance:
+		var packed_scene:PackedScene = load("res://scene/piece_queen.tscn")
+		var instance:PieceInstance = packed_scene.instantiate()
+		instance.position_name = position_name
+		instance.group = group
+		return instance
+
+	static func execute_navi(state:ChessState, position_name_from:String, position_name_to:String) -> void:
+		if state.has_piece(position_name_to):
+			state.capture_piece(position_name_to)
+		state.move_piece(position_name_from, position_name_to)
+
+	static func get_valid_navi(state:ChessState, position_name_from:String) -> PackedStringArray:
+		var directions:PackedVector2Array = [Vector2i(-1, -1), Vector2i(-1, 0), Vector2i(-1, 1), Vector2i(0, -1), Vector2i(0, 1), Vector2i(1, -1), Vector2i(1, 0), Vector2i(1, 1)]
+		var answer:PackedStringArray = []
+		for iter:Vector2i in directions:
+			var position_name_to:String = Chess.direction_to(position_name_from, iter)
+			while position_name_to && (!state.has_piece(position_name_to) || state.get_piece(position_name_from).group != state.get_piece(position_name_to).group):
+				answer.push_back(position_name_to)
+				position_name_to = Chess.direction_to(position_name_to, iter)
+		return answer
+
+class PieceRook extends PieceInterface:
+	static func create_instance(position_name:String, group:int) -> PieceInstance:
+		var packed_scene:PackedScene = load("res://scene/piece_rook.tscn")
+		var instance:PieceInstance = packed_scene.instantiate()
+		instance.position_name = position_name
+		instance.group = group
+		return instance
+
+	static func execute_navi(state:ChessState, position_name_from:String, position_name_to:String) -> void:
+		if state.has_piece(position_name_to):
+			state.capture_piece(position_name_to)
+		state.move_piece(position_name_from, position_name_to)
+
+	static func get_valid_navi(state:ChessState, position_name_from:String) -> PackedStringArray:
+		var directions:PackedVector2Array = [Vector2i(-1, 0), Vector2i(0, -1), Vector2i(0, 1), Vector2i(1, 0)]
+		var answer:PackedStringArray = []
+		for iter:Vector2i in directions:
+			var position_name_to:String = Chess.direction_to(position_name_from, iter)
+			while position_name_to && (!state.has_piece(position_name_to) || state.get_piece(position_name_from).group != state.get_piece(position_name_to).group):
+				answer.push_back(position_name_to)
+				position_name_to = Chess.direction_to(position_name_to, iter)
+		return answer
+
+class PieceBishop extends PieceInterface:
+	static func create_instance(position_name:String, group:int) -> PieceInstance:
+		var packed_scene:PackedScene = load("res://scene/piece_bishop.tscn")
+		var instance:PieceInstance = packed_scene.instantiate()
+		instance.position_name = position_name
+		instance.group = group
+		return instance
+
+	static func execute_navi(state:ChessState, position_name_from:String, position_name_to:String) -> void:
+		if state.has_piece(position_name_to):
+			state.capture_piece(position_name_to)
+		state.move_piece(position_name_from, position_name_to)
+
+	static func get_valid_navi(state:ChessState, position_name_from:String) -> PackedStringArray:
+		var directions:PackedVector2Array = [Vector2i(-1, -1), Vector2i(-1, 0), Vector2i(-1, 1), Vector2i(0, -1), Vector2i(0, 1), Vector2i(1, -1), Vector2i(1, 0), Vector2i(1, 1)]
+		var answer:PackedStringArray = []
+		for iter:Vector2i in directions:
+			var position_name_to:String = Chess.direction_to(position_name_from, iter)
+			while position_name_to && (!state.has_piece(position_name_to) || state.get_piece(position_name_from).group != state.get_piece(position_name_to).group):
+				answer.push_back(position_name_to)
+				position_name_to = Chess.direction_to(position_name_to, iter)
+		return answer
+
+class PieceKnight extends PieceInterface:
+	static func create_instance(position_name:String, group:int) -> PieceInstance:
+		var packed_scene:PackedScene = load("res://scene/piece_knight.tscn")
+		var instance:PieceInstance = packed_scene.instantiate()
+		instance.position_name = position_name
+		instance.group = group
+		return instance
+
+	static func execute_navi(state:ChessState, position_name_from:String, position_name_to:String) -> void:
+		if state.has_piece(position_name_to):
+			state.capture_piece(position_name_to)
+		state.move_piece(position_name_from, position_name_to)
+
+	static func get_valid_navi(state:ChessState, position_name_from:String) -> PackedStringArray:
+		var directions:PackedVector2Array = [Vector2i(1, 2), Vector2i(2, 1), Vector2i(-1, 2), Vector2i(-2, 1), Vector2i(1, -2), Vector2i(2, -1), Vector2i(-1, -2), Vector2i(-2, -1)]
+		var answer:PackedStringArray = []
+		for iter:Vector2i in directions:
+			var position_name_to:String = Chess.direction_to(position_name_from, iter)
+			if !position_name_to || state.has_piece(position_name_to) && state.get_piece(position_name_from).group == state.get_piece(position_name_to).group:
+				continue
+			answer.push_back(position_name_to)
+		return answer
+
+class PiecePawn extends PieceInterface:
+	static func create_instance(position_name:String, group:int) -> PieceInstance:
+		var packed_scene:PackedScene = load("res://scene/piece_pawn.tscn")
+		var instance:PieceInstance = packed_scene.instantiate()
+		instance.position_name = position_name
+		instance.group = group
+		return instance
+
+	static func execute_navi(state:ChessState, position_name_from:String, position_name_to:String) -> void:
+		if state.has_piece(position_name_to):
+			state.capture_piece(position_name_to)
+		state.move_piece(position_name_from, position_name_to)
+
+	static func get_valid_navi(state:ChessState, position_name_from:String) -> PackedStringArray:
+		var answer:PackedStringArray = []
+		var forward:Vector2i = Vector2i(0, 1) if state.get_piece(position_name_from).group == 0 else Vector2i(0, -1)
+		var on_start:bool = state.get_piece(position_name_from).group == 0 && position_name_from[1] == "2" || state.get_piece(position_name_from).group == 1 && position_name_from[1] == "7"
+		var position_name_to:String = Chess.direction_to(position_name_from, forward)
+		var position_name_to_2:String = Chess.direction_to(position_name_from, forward * 2)
+		var position_name_to_l:String = Chess.direction_to(position_name_to, Vector2i(1, 0))
+		var position_name_to_r:String = Chess.direction_to(position_name_to, Vector2i(-1, 0))
+		if !state.has_piece(position_name_to):
+			answer.push_back(position_name_to)
+			if on_start && !state.has_piece(position_name_to_2):
+				answer.push_back(position_name_to_2)
+		if state.has_piece(position_name_to_l) && state.get_piece(position_name_from).group != state.get_piece(position_name_to_l).group:
+			answer.push_back(position_name_to_l)
+		if state.has_piece(position_name_to_r) && state.get_piece(position_name_from).group != state.get_piece(position_name_to_r).group:
+			answer.push_back(position_name_to_r)
+		return answer
+
 class ChessState:
 	var state_name:String = "test"
 	var current:Dictionary[String, Piece] = {}
-	var history:PackedStringArray = []
+	var history:PackedStringArray = []	# 仅记录着法
+	var history_buffer:Array = []	# 详细记录所有变动
 	func _init() -> void:
 		current = {
+			"a1": Chess.create_piece(PieceRook, 0),
+			"b1": Chess.create_piece(PieceKnight, 0),
+			"c1": Chess.create_piece(PieceBishop, 0),
+			"d1": Chess.create_piece(PieceQueen, 0),
 			"e1": Chess.create_piece(PieceKing, 0),
-			"e8": Chess.create_piece(PieceKing, 8)
+			"f1": Chess.create_piece(PieceBishop, 0),
+			"g1": Chess.create_piece(PieceKnight, 0),
+			"h1": Chess.create_piece(PieceRook, 0),
+			"a2": Chess.create_piece(PiecePawn, 0),
+			"b2": Chess.create_piece(PiecePawn, 0),
+			"c2": Chess.create_piece(PiecePawn, 0),
+			"d2": Chess.create_piece(PiecePawn, 0),
+			"e2": Chess.create_piece(PiecePawn, 0),
+			"f2": Chess.create_piece(PiecePawn, 0),
+			"g2": Chess.create_piece(PiecePawn, 0),
+			"h2": Chess.create_piece(PiecePawn, 0),
+			"a8": Chess.create_piece(PieceRook, 1),
+			"b8": Chess.create_piece(PieceKnight, 1),
+			"c8": Chess.create_piece(PieceBishop, 1),
+			"d8": Chess.create_piece(PieceQueen, 1),
+			"e8": Chess.create_piece(PieceKing, 1),
+			"f8": Chess.create_piece(PieceBishop, 1),
+			"g8": Chess.create_piece(PieceKnight, 1),
+			"h8": Chess.create_piece(PieceRook, 1),
+			"a7": Chess.create_piece(PiecePawn, 1),
+			"b7": Chess.create_piece(PiecePawn, 1),
+			"c7": Chess.create_piece(PiecePawn, 1),
+			"d7": Chess.create_piece(PiecePawn, 1),
+			"e7": Chess.create_piece(PiecePawn, 1),
+			"f7": Chess.create_piece(PiecePawn, 1),
+			"g7": Chess.create_piece(PiecePawn, 1),
+			"h7": Chess.create_piece(PiecePawn, 1),
 		}
 	func get_piece_instance(position_name:String) -> PieceInstance:
 		var instance:PieceInstance = current[position_name].instance
@@ -63,6 +217,11 @@ class ChessState:
 		if !position_name || !current.has(position_name):
 			return null
 		return current[position_name]
+
+	func is_navi_valid(position_name_from:String, position_name_to:String) -> bool:
+		if !position_name_from || !position_name_to || !current.has(position_name_from):
+			return false
+		return get_valid_navi(position_name_from).has(position_name_to)
 
 	func get_valid_navi(position_name_from:String) -> PackedStringArray:
 		if has_piece(position_name_from):
@@ -120,92 +279,3 @@ func set_current_chessboard(_chessboard:Chessboard) -> void:
 
 func get_chess_state() -> ChessState:
 	return test_state
-
-#func get_valid_navi_queen(chessboard_name:String, position_name_from:String) -> PackedStringArray:
-#	var directions:PackedVector2Array = [Vector2i(-1, -1), Vector2i(-1, 0), Vector2i(-1, 1), Vector2i(0, -1), Vector2i(0, 1), Vector2i(1, -1), Vector2i(1, 0), Vector2i(1, 1)]
-#	var answer:PackedStringArray = []
-#	for iter:Vector2i in directions:
-#		var position_name_to:String = direction_to(position_name_from, iter)
-#		while !(!position_name_to || has_piece(chessboard_name, position_name_to) && pieces[chessboard_name][position_name_from]["group"] == pieces[chessboard_name][position_name_to]["group"]):
-#			answer.push_back(position_name_to)
-#			position_name_to = direction_to(position_name_to, iter)
-#	return answer
-#
-#func get_valid_navi_rook(chessboard_name:String, position_name_from:String) -> PackedStringArray:
-#	var directions:PackedVector2Array = [Vector2i(-1, 0), Vector2i(0, -1), Vector2i(0, 1), Vector2i(1, 0)]
-#	var answer:PackedStringArray = []
-#	for iter:Vector2i in directions:
-#		var position_name_to:String = direction_to(position_name_from, iter)
-#		while !(!position_name_to || has_piece(chessboard_name, position_name_to) && pieces[chessboard_name][position_name_from]["group"] == pieces[chessboard_name][position_name_to]["group"]):
-#			answer.push_back(position_name_to)
-#			position_name_to = direction_to(position_name_to, iter)
-#	return answer
-#
-#func get_valid_navi_bishop(chessboard_name:String, position_name_from:String) -> PackedStringArray:
-#	var directions:PackedVector2Array = [Vector2i(-1, -1), Vector2i(-1, 1), Vector2i(1, -1), Vector2i(1, 1)]
-#	var answer:PackedStringArray = []
-#	for iter:Vector2i in directions:
-#		var position_name_to:String = direction_to(position_name_from, iter)
-#		while !(!position_name_to || has_piece(chessboard_name, position_name_to) && pieces[chessboard_name][position_name_from]["group"] == pieces[chessboard_name][position_name_to]["group"]):
-#			answer.push_back(position_name_to)
-#			position_name_to = direction_to(position_name_to, iter)
-#	return answer
-#
-#func get_valid_navi_knight(chessboard_name:String, position_name_from:String) -> PackedStringArray:
-#	var directions:PackedVector2Array = [Vector2i(1, 2), Vector2i(2, 1), Vector2i(-1, 2), Vector2i(-2, 1), Vector2i(1, -2), Vector2i(2, -1), Vector2i(-1, -2), Vector2i(-2, -1)]
-#	var answer:PackedStringArray = []
-#	for iter:Vector2i in directions:
-#		var position_name_to:String = direction_to(position_name_from, iter)
-#		if !position_name_to || has_piece(chessboard_name, position_name_to) && pieces[chessboard_name][position_name_from]["group"] == pieces[chessboard_name][position_name_to]["group"]:
-#			continue
-#		answer.push_back(position_name_to)
-#	return answer
-#
-#func get_valid_navi_pawn(chessboard_name:String, position_name_from:String) -> PackedStringArray:
-#	var answer:PackedStringArray = []
-#	var forward:Vector2i = Vector2i(0, 1) if pieces[chessboard_name][position_name_from]["group"] == 0 else Vector2i(0, -1)
-#	var on_start:bool = pieces[chessboard_name][position_name_from]["group"] == 0 && position_name_from[1] == "2" || pieces[chessboard_name][position_name_from]["group"] == 1 && position_name_from[1] == "7"
-#	var position_name_to:String = direction_to(position_name_from, forward)
-#	var position_name_to_2:String = direction_to(position_name_from, forward * 2)
-#	var position_name_to_l:String = direction_to(position_name_to, Vector2i(1, 0))
-#	var position_name_to_r:String = direction_to(position_name_to, Vector2i(-1, 0))
-#	if !has_piece(chessboard_name, position_name_to):
-#		answer.push_back(position_name_to)
-#		if on_start && !has_piece(chessboard_name, position_name_to_2):
-#			answer.push_back(position_name_to_2)
-#	if has_piece(chessboard_name, position_name_to_l) && pieces[chessboard_name][position_name_from]["group"] != pieces[chessboard_name][position_name_to_l]["group"]:
-#		answer.push_back(position_name_to_l)
-#	if has_piece(chessboard_name, position_name_to_r) && pieces[chessboard_name][position_name_from]["group"] != pieces[chessboard_name][position_name_to_r]["group"]:
-#		answer.push_back(position_name_to_r)
-#	return answer
-#
-#func execute_navi_king(chessboard_name:String, position_name_from:String, position_name_to:String) -> void:
-#	if Chess.has_piece(chessboard_name, position_name_to):
-#		Chess.capture_piece(chessboard_name, position_name_to)
-#	Chess.move_piece(chessboard_name, position_name_from, position_name_to)
-#
-#func execute_navi_queen(chessboard_name:String, position_name_from:String, position_name_to:String) -> void:
-#	if Chess.has_piece(chessboard_name, position_name_to):
-#		Chess.capture_piece(chessboard_name, position_name_to)
-#	Chess.move_piece(chessboard_name, position_name_from, position_name_to)
-#
-#func execute_navi_rook(chessboard_name:String, position_name_from:String, position_name_to:String) -> void:
-#	if Chess.has_piece(chessboard_name, position_name_to):
-#		Chess.capture_piece(chessboard_name, position_name_to)
-#	Chess.move_piece(chessboard_name, position_name_from, position_name_to)
-#
-#func execute_navi_bishop(chessboard_name:String, position_name_from:String, position_name_to:String) -> void:
-#	if Chess.has_piece(chessboard_name, position_name_to):
-#		Chess.capture_piece(chessboard_name, position_name_to)
-#	Chess.move_piece(chessboard_name, position_name_from, position_name_to)
-#
-#func execute_navi_knight(chessboard_name:String, position_name_from:String, position_name_to:String) -> void:
-#	if Chess.has_piece(chessboard_name, position_name_to):
-#		Chess.capture_piece(chessboard_name, position_name_to)
-#	Chess.move_piece(chessboard_name, position_name_from, position_name_to)
-#	
-#func execute_navi_pawn(chessboard_name:String, position_name_from:String, position_name_to:String) -> void:
-#	if Chess.has_piece(chessboard_name, position_name_to):
-#		Chess.capture_piece(chessboard_name, position_name_to)
-#	Chess.move_piece(chessboard_name, position_name_from, position_name_to)
-#
