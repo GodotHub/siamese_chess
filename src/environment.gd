f1c1b8551df95b3b73f654c5f7be6bf8986915f5
FaMuLan <fa_mulan@163.com>
2025-05-16 14:51:43 +0800

    注意更改一下节点名称

--- src/environment.gd
+++ src/environment.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
 func _ready() -> void:
-	$player.connect("tap_position", $chessboard.tap_position)
-	$player.connect("finger_on_position", $chessboard.finger_on_position)
-	$player.connect("finger_up", $chessboard.finger_up)
+	$cheshire.connect("tap_position", $chessboard.tap_position)
+	$cheshire.connect("finger_on_position", $chessboard.finger_on_position)
+	$cheshire.connect("finger_up", $chessboard.finger_up)
 	$chessboard.connect("move_played", $history.push_move)
 	$chessboard.connect("move_played", $pastor.receive_move)
 	$pastor.connect("send_initial_state", $chessboard.set_state)
