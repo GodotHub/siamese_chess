2ab2163554ae8ada0c80c258890a4098a13e7584
FaMuLan <fa_mulan@163.com>
2025-05-21 20:09:06 +0800

    空着裁剪，思考层数增加了两层

--- src/core/chess_branch.gd
+++ src/core/chess_branch.gd
@@ -2,11 +2,17 @@ extends Object
 class_name ChessBranch
 
 func alphabeta(_state:ChessState, alpha:float, beta:float, depth:int = 5, group:int = 0) -> float:
-	if !depth:
+	if depth <= 0:	# 底端
 		return _state.score
+
 	var move_to_state:Dictionary[Move, ChessState] = {}
 	var move_list:Array[Move] = []
 	if group == 0:
+		# 空着裁剪
+		var null_move_value:float = alphabeta(_state, beta - 1, beta, depth - 1 - 3, 1)
+		if null_move_value >= beta:
+			return beta
+
 		move_list = _state.get_all_move(group)
 		var value:float = -10000
 		for iter:Move in move_list:
@@ -21,6 +27,11 @@ func alphabeta(_state:ChessState, alpha:float, beta:float, depth:int = 5, group:
 				break
 		return value
 	else:
+		# 空着裁剪
+		var null_move_value:float = alphabeta(_state, alpha, alpha + 1, depth - 1 - 3, 0)
+		if null_move_value <= alpha:
+			return alpha
+
 		move_list = _state.get_all_move(group)
 		var value:float = 10000
 		for iter:Move in move_list:
@@ -41,5 +52,8 @@ func search(state:ChessState, depth:int = 10, group:int = 0) -> Dictionary:
 	for iter:Move in move_list:
 		var test_state:ChessState = state.duplicate()
 		test_state.execute_move(iter)
+		var valid_check:float = alphabeta(test_state, -10000, 10000, 1, 1 if group == 0 else 0)	# 下一步被吃就说明这一步不合法
+		if abs(valid_check) >= 500:
+			continue
 		output[iter.stringify()] = alphabeta(test_state, -10000, 10000, depth - 1, 1 if group == 0 else 0)
 	return output
