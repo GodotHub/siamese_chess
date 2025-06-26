688207f55bcf1662edd1b65696d5fc176b5394b2
FaMuLan <fa_mulan@163.com>
2025-06-26 18:36:03 +0800

    随机数保存，以及更快的截止时间

--- src/core/evaluation_standard.gd
+++ src/core/evaluation_standard.gd
@@ -524,7 +524,7 @@ static func compare_move(a:int, b:int, _group:int, move_to_score:Dictionary, his
 		return history_table.get(a, 0) > history_table.get(b, 0)
 	return move_to_score[a] > move_to_score[b]
 
-static func alphabeta(_state:ChessState, alpha:int, beta:int, depth:int = 5, group:int = 0, history_table:Dictionary[int, int] = {}, main_variation:PackedInt32Array = [], transposition_table:TranspositionTable = null) -> int:
+static func alphabeta(_state:ChessState, alpha:int, beta:int, depth:int = 5, group:int = 0, history_table:Dictionary[int, int] = {}, main_variation:PackedInt32Array = [], transposition_table:TranspositionTable = null, is_timeup:Callable = Callable()) -> int:
 	if is_instance_valid(transposition_table):
 		var score:int = transposition_table.probe_hash(_state.zobrist, depth, alpha, beta)
 		if score != 65535:
@@ -533,8 +533,13 @@ static func alphabeta(_state:ChessState, alpha:int, beta:int, depth:int = 5, gro
 		if is_instance_valid(transposition_table):
 			transposition_table.record_hash(_state.zobrist, depth, _state.get_relative_score(group), TranspositionTable.Flag.EXACT)
 		return _state.get_relative_score(group)
+
 	if _state.history.has(_state.zobrist):
 		return 0	# 视作平局，如果局面不太好，也不会选择负分的下法
+
+	if is_timeup.is_valid() && is_timeup.call():
+		return _state.get_relative_score(group)
+
 	var line:PackedInt32Array = []
 	var move_list:Array = []
 	var flag:TranspositionTable.Flag = TranspositionTable.Flag.ALPHA
@@ -544,7 +549,7 @@ static func alphabeta(_state:ChessState, alpha:int, beta:int, depth:int = 5, gro
 	for iter:int in move_list:
 		var test_state:ChessState = _state.duplicate()
 		test_state.apply_move(iter)
-		value = -alphabeta(test_state, -beta, -alpha, depth - 1, 1 - group, history_table, line, transposition_table)
+		value = -alphabeta(test_state, -beta, -alpha, depth - 1, 1 - group, history_table, line, transposition_table, is_timeup)
 		if beta <= value:
 			if is_instance_valid(transposition_table):
 				transposition_table.record_hash(_state.zobrist, depth, _state.get_relative_score(group), TranspositionTable.Flag.BETA)
@@ -593,7 +598,5 @@ static func search(state:ChessState, group:int, main_variation:PackedInt32Array 
 	# 迭代加深，并准备提前中断
 	var history_table:Dictionary[int, int] = {}
 	for i:int in range(1, 1000, 1):
-		mtdf(state, group, i, history_table, main_variation, transposition_table)
-		#alphabeta(state, -WIN, WIN, i, group, history_table, main_variation, transposition_table)
-		if is_timeup.call():
-			return
+		#mtdf(state, group, i, history_table, main_variation, transposition_table)
+		alphabeta(state, -WIN, WIN, i, group, history_table, main_variation, transposition_table, is_timeup)
