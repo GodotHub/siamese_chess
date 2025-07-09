4c09672f8e7d5b191a32547bce72a6b7247de69e
FaMuLan <fa_mulan@163.com>
2025-07-09 23:07:51 +0800

    不采用断言，直接测试所有样例

--- src/database_generator.gd
+++ src/database_generator.gd
@@ -30,39 +30,38 @@ func perft_test() -> void:
 	var node_count:PackedInt32Array = [1, 20, 400, 8902, 197281]#, 4865609, 119060324]
 	for i:int in range(node_count.size()):
 		var result:int = rule_standard.perft(chess_state, i, 0)
-		print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
-		assert(result == node_count[i])
+		if result != node_count[i]:
+			print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
 	chess_state = rule_standard.parse("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1")
 	node_count = [1, 48, 2039, 97862, 4085603]#, 193690690]
 	for i:int in range(node_count.size()):
 		var result:int = rule_standard.perft(chess_state, i, 0)
-		print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
-		assert(result == node_count[i])
+		if result != node_count[i]:
+			print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
 	chess_state = rule_standard.parse("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1")
 	node_count = [1, 14, 191, 2812, 43238, 67624]#, 11030083]
 	for i:int in range(node_count.size()):
 		var result:int = rule_standard.perft(chess_state, i, 0)
-		print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
-		assert(result == node_count[i])
+		if result != node_count[i]:
+			print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
 	chess_state = rule_standard.parse("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1")
 	node_count = [1, 6, 264, 9467, 422333]#, 15833292]
 	for i:int in range(node_count.size()):
 		var result:int = rule_standard.perft(chess_state, i, 0)
-		print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
-		assert(result == node_count[i])
+		if result != node_count[i]:
+			print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
 	chess_state = rule_standard.parse("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1")
 	node_count = [1, 6, 264, 9467, 422333]#, 15833292]
 	for i:int in range(node_count.size()):
-		var result:int = rule_standard.perft(chess_state, i, 1)
-		print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
-		assert(result == node_count[i])
-	
+		var result:int = rule_standard.perft(chess_state, i, 0)
+		if result != node_count[i]:
+			print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
 	chess_state = rule_standard.parse("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8")
 	node_count = [1, 44, 1486, 62379, 2103487]#, 89941194]
 	for i:int in range(node_count.size()):
 		var result:int = rule_standard.perft(chess_state, i, 0)
-		print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
-		assert(result == node_count[i])
+		if result != node_count[i]:
+			print("perft_test depth:%d expect:%d actual:%d" % [i, node_count[i], result])
 	
 func _physics_process(_delta:float):
 	while progress_bar_data.size() > progress_bar.size():
