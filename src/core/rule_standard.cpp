bef1a8f6973383989fe5758dc2b2559f26b7e7c2
FaMuLan <fa_mulan@163.com>
2025-07-09 21:44:05 +0800

    修正因为吃子评分导致的王车易位错误

--- src/core/rule_standard.cpp
+++ src/core/rule_standard.cpp
@@ -857,7 +857,7 @@ int RuleStandard::evaluate(godot::Ref<State>_state, int _move)
 	{
 		score -= get_piece_score(to, to_piece);
 	}
-	if (_state->get_extra(5) != -1 && abs(abs(_state->get_extra(5) - Chess::to(_move))) <= 1)
+	if (_state->get_extra(5) != -1 && abs(_state->get_extra(5) - Chess::to(_move)) <= 1 && (to_piece & 95) != 'K')
 	{
 		score -= piece_value[group == 0 ? 'k' : 'K'];
 	}
