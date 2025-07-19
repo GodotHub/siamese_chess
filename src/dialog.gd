255ce06f1b17e52f3fc7b332905fe3aafe334feb
FaMuLan <fa_mulan@163.com>
2025-07-19 19:25:33 +0800

    让对话框在场景内全局出现

--- src/dialog.gd
+++ src/dialog.gd
@@ -1,52 +1,69 @@
 extends CanvasLayer
 class_name Dialog
 
-signal on_next(index:int)
-signal on_exit()
-
-var text_list:PackedStringArray = [
-#	"现在你看到的是测试对话。",
-#	"你我之间对话通常不止一句，",
-#	"在对话同时，还涉及到视角的转换、角色神情的变化，",
-#	"甚至需要展示特定的图像，还有不同选项时的分支",
-#	"Dialog对象只处理对话的内容，",
-#	"在你点击时，除了跳到下一条语句外，它会发送信号，通知给其他演出相关的对象。",
-]
-var pointer:int = 0
+signal on_next()
 
 const packed_scene:PackedScene = preload("res://scene/dialog.tscn")
-
-static func create_dialog_instance(_text_list:PackedStringArray) -> Dialog:
-	var instance:Dialog = packed_scene.instantiate()
-	instance.text_list = _text_list
-	return instance
+var selected:String = ""
+var click_anywhere:bool = false
+var force_selection:bool = false
 
 func _ready() -> void:
+	$texture_rect_bottom/label.connect("meta_clicked", clicked_selection)
+	
+func test() -> void:
+	push_dialog("这是一段测试对话", true, true)
+	await on_next
+	push_dialog("这次使用全局对话框，可能会占用一部分的画面", true, true)
+	await on_next
+	push_dialog("不过，我们会在操作时穿插对话、注解以及选项", true, true)
+	await on_next
+	push_dialog("现在进行3秒间隔测试，该对话不可跳过", true, false)
+	await get_tree().create_timer(3).timeout
+	push_dialog("接下来是选项", false, true)
+	await on_next
+	push_selection(["选项1", "选项2", "选项3"], false, false)
+	await on_next
+	push_selection(["选项4", "选项5", "选项6"], true, true)
+
+
+func _unhandled_input(event:InputEvent) -> void:
+	if click_anywhere:
+		if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+			next()
+	if click_anywhere || force_selection:
+		get_viewport().set_input_as_handled()
+
+func push_dialog(text:String, blackscreen:bool = false, _click_anywhere:bool = false) -> void:
 	var tween:Tween = create_tween()
-	tween.tween_property($texture_rect_full, "visible", true, 0)
+	force_selection = false
+	click_anywhere = _click_anywhere
+	$texture_rect_bottom/label.text = ""
+	if blackscreen:
+		tween.tween_property($texture_rect_full, "visible", true, 0)
 	tween.tween_interval(0.3)
-	tween.tween_property($texture_rect_bottom/label, "text", text_list[pointer], 0)
+	tween.tween_property($texture_rect_bottom/label, "text", tr(text), 0)
 	tween.tween_property($texture_rect_full, "visible", false, 0)
 
-func _input(event:InputEvent) -> void:
-	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-		next()
-	get_viewport().set_input_as_handled()
+func push_selection(selection:PackedStringArray, _force_selection:bool = true, blackscreen:bool = false) -> void:
+	var text = ""
+	click_anywhere = false
+	force_selection = _force_selection
+	for iter:String in selection:
+		text += "[url link=\"" + iter + "\"]" + tr(iter) + "[/url]  "
+	var tween:Tween = create_tween()
+	if blackscreen:
+		tween.tween_property($texture_rect_full, "visible", true, 0)
+	tween.tween_interval(0.3)
+	tween.tween_property($texture_rect_bottom/label, "text", text, 0)
+	tween.tween_property($texture_rect_full, "visible", false, 0)
 
 func next() -> void:
-	pointer += 1
-	if pointer < text_list.size():
-		var tween:Tween = create_tween()
-		tween.tween_property($texture_rect_full, "visible", true, 0)
-		tween.tween_callback(on_next.emit.bind(pointer))
-		tween.tween_interval(0.3)
-		tween.tween_property($texture_rect_bottom/label, "text", text_list[pointer], 0)
-		tween.tween_property($texture_rect_full, "visible", false, 0)
-	else:
-		var tween:Tween = create_tween()
-		tween.tween_property($texture_rect_bottom/label, "visible", true, 0)
-		tween.tween_callback(on_next.emit.bind(pointer))
-		tween.tween_callback(on_exit.emit)
-		tween.tween_interval(0.3)
-		tween.tween_callback(queue_free)
-	
+	$texture_rect_bottom/label.text = ""
+	click_anywhere = false
+	force_selection = false
+	on_next.emit()
+
+func clicked_selection(_selected:String) -> void:
+	selected = _selected
+	next()
--- src/environment.gd
+++ src/environment.gd
@@ -27,151 +27,48 @@ func select_dialog() -> void:
 	pass
 
 func dialog_start_game() -> void:
-	var dialog_1:Dialog = Dialog.create_dialog_instance([
-		"现在你有若干选项开始游戏",
-	])
-	add_child(dialog_1)
+	$dialog.push_dialog("现在你有若干选项开始游戏", true, true)
 	$cheshire.force_set_camera($camera/camera_pastor_closeup)
-	await dialog_1.on_next
+	await $dialog.on_next
 	start()
 
 func timeout(group:int) -> void:
 	if group == 0:	# 棋钟的阵营1才是Pastor的
-		var dialog_1:Dialog = Dialog.create_dialog_instance([
-			"时间到。",
-			"这不是什么稀罕事，巨大压力之下普通人多少会不知所措。",
-			"能做的就是继续磨练自身的技术了",
-			"还需要再来一局吗？"
-		])
-		add_child(dialog_1)
-		await dialog_1.on_next
-		await dialog_1.on_next
-		await dialog_1.on_next
-		await dialog_1.on_next
+		$dialog.push_dialog("棋局结束，黑方超时", true, true)
+		await $dialog.on_next
 	else:
-		var dialog_1:Dialog = Dialog.create_dialog_instance([
-			"……很意外",
-			"作为引擎竟然还能出现超时。",
-			"您不仅要在时间上领先于我，还需要在防止出错的情况下拖到最后一刻。",
-			"可谓技艺精湛，值得肯定。",
-			"是否需要重新开始？"
-		])
-		add_child(dialog_1)
-		await dialog_1.on_next
-		await dialog_1.on_next
-		await dialog_1.on_next
-		await dialog_1.on_next
-		await dialog_1.on_next
-	start()
+		$dialog.push_dialog("棋局结束，白方超时", true, true)
+		await $dialog.on_next
 
 func pastor_win() -> void:
 	$chess_timer.stop()
-	var dialog_1:Dialog = Dialog.create_dialog_instance([
-		"……",
-		"看来是我拿下了胜利。",
-		"我仍相信您的决策经过了深思熟虑，但是致命的错误仍摆在这里。",
-		"我暂时没有其他安排，需要重新进行一场对局吗？",
-	])
-	add_child(dialog_1)
-	$cheshire.force_set_camera($camera/camera_chessboard)
-	await dialog_1.on_next
-	$cheshire.force_set_camera($camera/camera_pastor_closeup)
-	await dialog_1.on_next
-	$cheshire.force_set_camera($camera/camera_chessboard_side)
-	await dialog_1.on_next
-	$cheshire.force_set_camera($camera/camera_on_seat)
-	await dialog_1.on_next
-	start()
+	$dialog.push_dialog("棋局结束，白方将杀胜利", true, true)
+	await $dialog.on_next
 
 func pastor_draw(type:int) -> void:	# 0:长将和 1:白方逼和 2:黑方逼和 3:50步和 4:子力不足
 	$chess_timer.stop()
 	match type:
 		0:
-			var dialog_1:Dialog = Dialog.create_dialog_instance([
-				"长将和棋。",
-				"看来你我都不认为这盘棋能继续下去，该说是错失良机，还是重获新生？",
-				"我暂时没有其他安排，需要重新进行一场对局吗？"
-			])
-			add_child(dialog_1)
-			$cheshire.force_set_camera($camera/camera_pastor_closeup)
-			await dialog_1.on_next
-			$cheshire.force_set_camera($camera/camera_chessboard_side)
-			await dialog_1.on_next
-			$cheshire.force_set_camera($camera/camera_on_seat)
-			await dialog_1.on_next
+			$dialog.push_dialog("长将和棋", true, true)
+			await $dialog.on_next
 		1:
-			var dialog_1:Dialog = Dialog.create_dialog_instance([
-				"逼和结束。",
-				"残局处理时，即使是您的优势，也需要谨慎落子，这跟靠蛮力打架可不一样。",
-				"我暂时没有其他安排，需要重新进行一场对局吗？"
-			])
-			add_child(dialog_1)
-			$cheshire.force_set_camera($camera/camera_pastor_closeup)
-			await dialog_1.on_next
-			$cheshire.force_set_camera($camera/camera_chessboard_side)
-			await dialog_1.on_next
-			$cheshire.force_set_camera($camera/camera_on_seat)
-			await dialog_1.on_next
+			$dialog.push_dialog("黑方逼和", true, true)
+			await $dialog.on_next
 		2:
-			var dialog_1:Dialog = Dialog.create_dialog_instance([
-				"逼和，但这对您而言是好事。",
-				"在我的巨大优势中找到强制和棋的着法，这是我的疏忽，也是您实力的体现。",
-				"我暂时没有其他安排，需要重新进行一场对局吗？"
-			])
-			add_child(dialog_1)
-			$cheshire.force_set_camera($camera/camera_pastor_closeup)
-			await dialog_1.on_next
-			$cheshire.force_set_camera($camera/camera_chessboard_side)
-			await dialog_1.on_next
-			$cheshire.force_set_camera($camera/camera_on_seat)
-			await dialog_1.on_next
+			$dialog.push_dialog("白方逼和", true, true)
+			await $dialog.on_next
 		3:
-			var dialog_1:Dialog = Dialog.create_dialog_instance([
-				"和棋，超出50个半步未吃子。",
-				"看来这场残局的推进还是蛮有难度的。",
-				"我暂时没有其他安排，需要重新进行一场对局吗？"
-			])
-			add_child(dialog_1)
-			$cheshire.force_set_camera($camera/camera_pastor_closeup)
-			await dialog_1.on_next
-			$cheshire.force_set_camera($camera/camera_chessboard_side)
-			await dialog_1.on_next
-			$cheshire.force_set_camera($camera/camera_on_seat)
-			await dialog_1.on_next
+			$dialog.push_dialog("50步和棋", true, true)
+			await $dialog.on_next
 		4:
-			var dialog_1:Dialog = Dialog.create_dialog_instance([
-				"……",
-				"和棋，这些棋子没有办法对双方的国王造成任何威胁。",
-				"能出现这种情况也非常罕见，只要有一个兵就有决定胜负的机会。",
-				"我暂时没有其他安排，需要重新进行一场对局吗？"
-			])
-			add_child(dialog_1)
-			$cheshire.force_set_camera($camera/camera_pastor_closeup)
-			await dialog_1.on_next
-			$cheshire.force_set_camera($camera/camera_chessboard_side)
-			await dialog_1.on_next
-			$cheshire.force_set_camera($camera/camera_on_seat)
-			await dialog_1.on_next
+			$dialog.push_dialog("子力不足和棋", true, true)
+			await $dialog.on_next
 	start()
 
 func pastor_lose() -> void:
 	$chess_timer.stop()
-	var dialog_1:Dialog = Dialog.create_dialog_instance([
-		"……",
-		"您在标准国际象棋规则中，拿下了胜利。",
-		"您的计算能力足够强大，或许真能解决您目前所发生的事态。",
-		"我暂时没有其他安排，需要重新进行一场对局吗？"
-	])
-	add_child(dialog_1)
-	$cheshire.force_set_camera($camera/camera_chessboard)
-	await dialog_1.on_next
-	$cheshire.force_set_camera($camera/camera_pastor_closeup)
-	await dialog_1.on_next
-	$cheshire.force_set_camera($camera/camera_chessboard_side)
-	await dialog_1.on_next
-	$cheshire.force_set_camera($camera/camera_on_seat)
-	await dialog_1.on_next
-	start()
+	$dialog.push_dialog("棋局结束，黑方将杀胜利", true, true)
+	await $dialog.on_next
 
 func start() -> void:
 	while true:
@@ -180,10 +77,6 @@ func start() -> void:
 		await decision_instance.decided
 		if decision_instance.selected_index in [0, 1, 2]:
 			$pastor.create_state("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", RuleStandard.new())
-			var dialog_2:Dialog = Dialog.create_dialog_instance([
-				"现在棋盘已经准备好了。",
-				"您是黑方后手开局，时间有限，请注意合理分配。"
-			])
 			if decision_instance.selected_index == 0:
 				$chess_timer.set_time(1800, 1, 0)
 				$pastor.think_time = 5
@@ -193,28 +86,26 @@ func start() -> void:
 			elif decision_instance.selected_index == 2:
 				$chess_timer.set_time(300, 1, 3)
 				$pastor.think_time = 2
-			add_child(dialog_2)
+			$dialog.push_dialog("现在棋盘已经准备好了。", true, true)
 			$cheshire.force_set_camera($camera/camera_chessboard)
-			await dialog_2.on_next
+			await $dialog.on_next
+			$dialog.push_dialog("您是黑方后手开局，时间有限，请注意合理分配。", true, true)
 			$cheshire.force_set_camera($camera/camera_pastor_closeup)
-			await dialog_2.on_next
+			await $dialog.on_next
 			$chess_timer.start()
 			$cheshire.add_stack($interact/area_chessboard)
 			$pastor.start_decision()
 			break
 		elif decision_instance.selected_index == 3:
 			$pastor.create_state("8/8/2rbqk2/2pppn2/2NPPP2/2KQBR2/8/8 w - - 0 1", RuleStandard.new())
-			var dialog_2:Dialog = Dialog.create_dialog_instance([
-				"现在棋盘已经准备好了。",
-				"该对局为特殊布局，时间上较为紧张，注意速战速决。"
-			])
 			$chess_timer.set_time(30, 1, 0)
 			$pastor.think_time = 1
-			add_child(dialog_2)
+			$dialog.push_dialog("现在棋盘已经准备好了。", true, true)
 			$cheshire.force_set_camera($camera/camera_chessboard)
-			await dialog_2.on_next
+			await $dialog.on_next
+			$dialog.push_dialog("该对局为特殊布局，时间上较为紧张，注意速战速决。", true, true)
 			$cheshire.force_set_camera($camera/camera_pastor_closeup)
-			await dialog_2.on_next
+			await $dialog.on_next
 			$chess_timer.start()
 			$cheshire.add_stack($interact/area_chessboard)
 			$pastor.start_decision()
@@ -224,19 +115,17 @@ func start() -> void:
 			add_child(text_input_instance)
 			await text_input_instance.confirmed
 			if $pastor.create_state(text_input_instance.text, RuleStandard.new()):
-				var dialog_2:Dialog = Dialog.create_dialog_instance([
-					"现在棋盘已经准备好了。",
-					"根据棋局信息，" + ("目前是白方先手。" if $pastor.state.get_extra(0) == 0 else "目前是黑方先手。")
-				])
 				$pastor.think_time = 10
-				add_child(dialog_2)
+				$dialog.push_dialog("现在棋盘已经准备好了。", true, true)
 				$cheshire.force_set_camera($camera/camera_chessboard)
-				await dialog_2.on_next
-				await dialog_2.on_next
+				await $dialog.on_next
+				$dialog.push_dialog("根据棋局信息，" + ("目前是白方先手。" if $pastor.state.get_extra(0) == 0 else "目前是黑方先手。"), true, true)
+				$cheshire.force_set_camera($camera/camera_pastor_closeup)
+				await $dialog.on_next
 				$cheshire.add_stack($interact/area_chessboard)
 				$pastor.start_decision()
 				break
+		$cheshire.force_set_camera($camera/camera_chessboard)	
+		$dialog.push_dialog("您输入的格式有误，请重新检查。", true, true)
 		$cheshire.force_set_camera($camera/camera_chessboard)
-		var dialog_illegal:Dialog = Dialog.create_dialog_instance(["您输入的格式有误，务必重新检查一下再尝试。"])
-		add_child(dialog_illegal)
-		await dialog_illegal.on_next
+		await $dialog.on_next
