728ee11248ef89f669e6048f4cf4754b17432916
FaMuLan <fa_mulan@163.com>
2025-07-25 21:39:23 +0800

    为棋子添加材质，虽然不明显

--- src/piece_instance.gd
+++ src/piece_instance.gd
@@ -18,17 +18,22 @@ func _ready() -> void:
 		position_name = chessboard.get_position_name(position)
 		position = chessboard.convert_name_to_position(position_name)
 		
-	var material:StandardMaterial3D = StandardMaterial3D.new()
+	var material:ShaderMaterial = ShaderMaterial.new()
+	material.shader = load("res://src/unmoving_plaid.gdshader")
+	material.set_shader_parameter("texture_albedo", load("res://assets/texture/Texturelabs_Stone_131S.jpg"))
+	material.set_shader_parameter("saturation_mult", 0.85)
+	material.set_shader_parameter("value_mult", 0.728)
+	material.set_shader_parameter("contrast_mult", 0.199)
 	var next_pass_material:StandardMaterial3D = StandardMaterial3D.new()
 	next_pass_material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
 	next_pass_material.cull_mode = BaseMaterial3D.CULL_FRONT
 	next_pass_material.grow = true
 	next_pass_material.grow_amount = 0.005
 	if group == 1:
-		material.albedo_color = Color(0.3, 0.3, 0.3, 1)
+		material.set_shader_parameter("brightness_add", 0.000)
 		next_pass_material.albedo_color = Color(1, 1, 1, 1)
 	else:
-		material.albedo_color = Color(0.7, 0.7, 0.7, 1)
+		material.set_shader_parameter("brightness_add", 0.676)
 		next_pass_material.albedo_color = Color(0, 0, 0, 1)
 	material.next_pass = next_pass_material
 	$piece.set_surface_override_material(0, material)
