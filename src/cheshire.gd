586236607968c9f3224b2b6b5932032a469c4fe6
FaMuLan <fa_mulan@163.com>
2025-07-25 23:00:52 +0800

    添加点击移动时的风声

--- src/cheshire.gd
+++ src/cheshire.gd
@@ -55,6 +55,7 @@ func move_camera(other:Camera3D) -> void:
 	if !is_instance_valid(other):
 		return
 	var tween:Tween = create_tween()
+	tween.tween_callback($audio_stream_player.play)
 	tween.tween_property($head, "global_transform", other.global_transform, 1).set_trans(Tween.TRANS_SINE)
 	tween.set_parallel()
 	tween.tween_property($head/camera, "fov", other.fov, 1).set_trans(Tween.TRANS_SINE)
