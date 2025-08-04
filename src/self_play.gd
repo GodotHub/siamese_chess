a401a32d61f33fd3f995c2f17f6bf55b15dcb64e
JustDooooIt <844730658@qq.com>
2025-08-05 00:40:55 +0800

    将文件操作移动到cpp内

--- src/self_play.gd
+++ src/self_play.gd
@@ -2,8 +2,7 @@ extends Node3D
 
 var cur_player: int = 0
 var cur_epoch: int = 0
-var white: PastorAI = PastorAI.new()
-var black: PastorAI = PastorAI.new()
+var ai: PastorAI = PastorAI.new()
 var state: State = null
 
 @export var epoch: int = 0
@@ -11,15 +10,10 @@ var state: State = null
 
 func init_state() -> void:
 	state = RuleStandard.parse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-	if FileAccess.file_exists("user://standard_opening.fa"):
-		white.get_transposition_table().load_file("user://standard_opening.fa")
-	else:
-		white.get_transposition_table().reserve(1 << 20)
 	chessboard.set_state(state.duplicate())
 
 func init_ai() -> void:
-	white.max_depth = 5
-	black.max_depth = 5
+	ai.max_depth = 5
 
 func on_play_pressed() -> void:
 	init_state()
