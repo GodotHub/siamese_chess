7f3253598bb8fa5ca409200ef2de9480a338d122
FaMuLan <fa_mulan@163.com>
2025-08-14 15:30:05 +0800

    唱片机的播放及其互动

---
+++ src/phonograph.gd
@@ -0,0 +1,62 @@
+extends InspectableItem
+
+@export var audio_stream:AudioStream = null
+var source_frequency:int = 44100
+var wav:PackedVector2Array = []
+var generator_playback:AudioStreamGeneratorPlayback = null
+@onready var audio_stream_player:AudioStreamPlayer3D = $audio_stream_player_3d
+@onready var area_stylus:Area3D = $area_stylus	# 点击唱针播放
+@onready var area_vinyl:Area3D = $area_vinyl	# 点击唱片切换？或者搓碟也不是不行？
+var polar_position:Vector2 = Vector2()
+var last_event_polar_position:Vector2 = Vector2()
+var play_frame:int = 0	# 下标位置
+var frame_delta:int = 0	# 这一帧跳转到这里
+var playing:bool = false
+
+func _ready() -> void:
+	super._ready()
+	var audio_stream_playback:AudioStreamPlayback = audio_stream.instantiate_playback()
+	
+	var pointer:float = 0
+	audio_stream_playback.start(0)
+	var data:PackedVector2Array = audio_stream_playback.mix_audio(1, 44100)
+	while data.size():
+		wav.append_array(data)
+		data = audio_stream_playback.mix_audio(1, 44100)
+	source_frequency = wav.size() / audio_stream.get_length()
+	generator_playback = audio_stream_player.get_stream_playback()
+
+func _process(delta: float) -> void:
+	if playing:
+		frame_delta += source_frequency * delta
+	if last_event_polar_position != Vector2.ZERO:
+		var angle_velocity:float = angle_difference(last_event_polar_position.y, polar_position.y)
+		frame_delta = 44100 * angle_velocity / ((TAU * 100.0 / 3.0) / 60.0)
+	last_event_polar_position = polar_position
+	fill_buffer(delta)
+
+func input(_from:Node3D, _to:Area3D, _event:InputEvent, _event_position:Vector3, _normal:Vector3) -> void:
+	if _to == area_stylus:
+		if _event is InputEventMouseButton:
+			if _event.pressed && _event.button_index == MOUSE_BUTTON_LEFT:
+				playing = !playing
+	elif _to == area_vinyl:
+		if audio_stream_player.playing:
+			var collision_shape:CollisionShape3D = area_vinyl.get_node("collision_shape_3d")
+			var event_position_3d:Vector3 = collision_shape.global_transform.affine_inverse() * _event_position
+			var event_position_2d:Vector2 = Vector2(event_position_3d.x, event_position_3d.z)
+			polar_position = Vector2(event_position_2d.length(), event_position_2d.angle())
+		else:
+			pass
+
+func fill_buffer(delta:float) -> void:
+	var frame_count:int = 44100 * delta
+	for i:int in frame_count:
+		var frame:int = play_frame + float(frame_delta) * float(i) / float(frame_count)
+		frame = clamp(frame, 0, wav.size() - 1)
+		if frame >= 0 && frame < wav.size():
+			generator_playback.push_frame(wav[frame])
+		else:
+			generator_playback.push_frame(Vector2(0, 0))
+	play_frame = clamp(play_frame + frame_delta, 0, wav.size() - 1)
+	frame_delta = 0
