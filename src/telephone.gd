cb9103089788dc7fb6346afa8d38c9ec02524cc8
FaMuLan <fa_mulan@163.com>
2025-08-18 23:19:23 +0800

    电话机

--- src/telephone.gd
+++ src/telephone.gd
@@ -8,18 +8,18 @@ var address:Array = [
 
 signal call_number(number:String)
 
-@onready var area_1:Area3D = $"1"
-@onready var area_2:Area3D = $"2"
-@onready var area_3:Area3D = $"3"
-@onready var area_4:Area3D = $"4"
-@onready var area_5:Area3D = $"5"
-@onready var area_6:Area3D = $"6"
-@onready var area_7:Area3D = $"7"
-@onready var area_8:Area3D = $"8"
-@onready var area_9:Area3D = $"9"
-@onready var area_0:Area3D = $"0"
-@onready var area_phone:Area3D = $"phone/area_phone"
-@onready var dial_mask:MeshInstance3D = $dial_base/dial_mask
+@onready var area_1:Area3D = $"area_1"
+@onready var area_2:Area3D = $"area_2"
+@onready var area_3:Area3D = $"area_3"
+@onready var area_4:Area3D = $"area_4"
+@onready var area_5:Area3D = $"area_5"
+@onready var area_6:Area3D = $"area_6"
+@onready var area_7:Area3D = $"area_7"
+@onready var area_8:Area3D = $"area_8"
+@onready var area_9:Area3D = $"area_9"
+@onready var area_0:Area3D = $"area_0"
+@onready var area_phone:Area3D = $"microphone/area_3d"
+@onready var dial_mask:MeshInstance3D = $dial_base/dial
 
 var number:String = ""
 var hanging:bool = false
@@ -36,44 +36,54 @@ func input(_from:Node3D, _to:Area3D, _event:InputEvent, _event_position:Vector3,
 	if _event is InputEventMouseButton && _event.pressed && _event.button_index == MOUSE_BUTTON_LEFT:
 		if _to == area_0:
 			number += "0"
-			create_tween().tween_property(dial_mask, "rotation:y", 30 * 10, 0.3).set_trans(Tween.TRANS_SINE)
-			create_tween().tween_property(dial_mask, "rotation:y", 0, 0.3).set_trans(Tween.TRANS_SINE)
+			var tween:Tween = create_tween()
+			tween.tween_property(dial_mask, "rotation_degrees:y", -320, 0.85).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(dial_mask, "rotation_degrees:y", 0, 0.85).set_trans(Tween.TRANS_SINE)
 		elif _to == area_1:
 			number += "1"
-			create_tween().tween_property(dial_mask, "rotation:y", 30 * 1, 0.3).set_trans(Tween.TRANS_SINE)
-			create_tween().tween_property(dial_mask, "rotation:y", 0, 0.3).set_trans(Tween.TRANS_SINE)
+			var tween:Tween = create_tween()
+			tween.tween_property(dial_mask, "rotation_degrees:y", -67, 0.4).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(dial_mask, "rotation_degrees:y", 0, 0.4).set_trans(Tween.TRANS_SINE)
 		elif _to == area_2:
 			number += "2"
-			create_tween().tween_property(dial_mask, "rotation:y", 30 * 2, 0.3).set_trans(Tween.TRANS_SINE)
-			create_tween().tween_property(dial_mask, "rotation:y", 0, 0.3).set_trans(Tween.TRANS_SINE)
+			var tween:Tween = create_tween()
+			tween.tween_property(dial_mask, "rotation_degrees:y", -96, 0.45).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(dial_mask, "rotation_degrees:y", 0, 0.45).set_trans(Tween.TRANS_SINE)
 		elif _to == area_3:
 			number += "3"
-			create_tween().tween_property(dial_mask, "rotation:y", 30 * 3, 0.3).set_trans(Tween.TRANS_SINE)
-			create_tween().tween_property(dial_mask, "rotation:y", 0, 0.3).set_trans(Tween.TRANS_SINE)
+			var tween:Tween = create_tween()
+			tween.tween_property(dial_mask, "rotation_degrees:y", -124, 0.5).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(dial_mask, "rotation_degrees:y", 0, 0.5).set_trans(Tween.TRANS_SINE)
 		elif _to == area_4:
 			number += "4"
-			create_tween().tween_property(dial_mask, "rotation:y", 30 * 4, 0.3).set_trans(Tween.TRANS_SINE)
-			create_tween().tween_property(dial_mask, "rotation:y", 0, 0.3).set_trans(Tween.TRANS_SINE)
+			var tween:Tween = create_tween()
+			tween.tween_property(dial_mask, "rotation_degrees:y", -150, 0.55).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(dial_mask, "rotation_degrees:y", 0, 0.55).set_trans(Tween.TRANS_SINE)
 		elif _to == area_5:
 			number += "5"
-			create_tween().tween_property(dial_mask, "rotation:y", 30 * 5, 0.3).set_trans(Tween.TRANS_SINE)
-			create_tween().tween_property(dial_mask, "rotation:y", 0, 0.3).set_trans(Tween.TRANS_SINE)
+			var tween:Tween = create_tween()
+			tween.tween_property(dial_mask, "rotation_degrees:y", -177, 0.6).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(dial_mask, "rotation_degrees:y", 0, 0.6).set_trans(Tween.TRANS_SINE)
 		elif _to == area_6:
 			number += "6"
-			create_tween().tween_property(dial_mask, "rotation:y", 30 * 6, 0.3).set_trans(Tween.TRANS_SINE)
-			create_tween().tween_property(dial_mask, "rotation:y", 0, 0.3).set_trans(Tween.TRANS_SINE)
+			var tween:Tween = create_tween()
+			tween.tween_property(dial_mask, "rotation_degrees:y", -206, 0.65).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(dial_mask, "rotation_degrees:y", 0, 0.65).set_trans(Tween.TRANS_SINE)
 		elif _to == area_7:
 			number += "7"
-			create_tween().tween_property(dial_mask, "rotation:y", 30 * 7, 0.3).set_trans(Tween.TRANS_SINE)
-			create_tween().tween_property(dial_mask, "rotation:y", 0, 0.3).set_trans(Tween.TRANS_SINE)
+			var tween:Tween = create_tween()
+			tween.tween_property(dial_mask, "rotation_degrees:y", -231, 0.7).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(dial_mask, "rotation_degrees:y", 0, 0.7).set_trans(Tween.TRANS_SINE)
 		elif _to == area_8:
 			number += "8"
-			create_tween().tween_property(dial_mask, "rotation:y", 30 * 8, 0.3).set_trans(Tween.TRANS_SINE)
-			create_tween().tween_property(dial_mask, "rotation:y", 0, 0.3).set_trans(Tween.TRANS_SINE)
+			var tween:Tween = create_tween()
+			tween.tween_property(dial_mask, "rotation_degrees:y", -258, 0.75).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(dial_mask, "rotation_degrees:y", 0, 0.75).set_trans(Tween.TRANS_SINE)
 		elif _to == area_9:
 			number += "9"
-			create_tween().tween_property(dial_mask, "rotation:y", 30 * 9, 0.3).set_trans(Tween.TRANS_SINE)
-			create_tween().tween_property(dial_mask, "rotation:y", 0, 0.3).set_trans(Tween.TRANS_SINE)
+			var tween:Tween = create_tween()
+			tween.tween_property(dial_mask, "rotation_degrees:y", -290, 0.8).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(dial_mask, "rotation_degrees:y", 0, 0.8).set_trans(Tween.TRANS_SINE)
 		elif _to == area_phone:
 			number = ""
 			hanging = !hanging
