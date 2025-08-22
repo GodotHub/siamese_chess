b100311fd74ac228d12fb846c2a220fa9cb726a9
FaMuLan <fa_mulan@163.com>
2025-08-22 22:45:09 +0800

    文档浏览器加载用户文件中的文档

--- src/archive.gd
+++ src/archive.gd
@@ -1,11 +1,25 @@
 extends CanvasLayer
 
-var document_list:Dictionary = {
-	"Notation": "res://scene/history.tscn",
-	"Menu": "res://scene/menu.tscn"
+var template_list:Dictionary = {
+	"menu": "res://scene/menu.tscn",
+	"history": "res://scene/history.tscn",
 }
 
+var document_list:Dictionary = {}
+
 func _ready() -> void:
+	var dir:DirAccess = DirAccess.open("user://archive/")
+	if !dir:
+		DirAccess.make_dir_absolute("user://archive/")
+		dir = DirAccess.open("user://archive/")
+	dir.list_dir_begin()
+	var file_name:String = dir.get_next()
+	while file_name != "":
+		if !dir.current_is_dir():
+			document_list[file_name] = "user://archive/" + file_name
+		file_name = dir.get_next()
+	
+
 	for iter:String in document_list:
 		var button = Button.new()
 		button.text = iter
@@ -24,4 +38,8 @@ func _ready() -> void:
 		$texture_rect/v_box_container.add_child(button)
 	
 func button_pressed(key:String) -> void:
-	$texture_rect/document_browser.set_document(load(document_list[key]).instantiate())
+	var filename_splited = key.split(".")	# 模板.名称.json
+	var new_document:Document = load(template_list[filename_splited[0]]).instantiate()
+	var data:String = FileAccess.get_file_as_string(document_list[key])
+	new_document.parse(data)
+	$texture_rect/document_browser.set_document(new_document)
