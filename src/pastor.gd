bcc22d528ab39e903cf92bd5321f843af868d372
FaMuLan <fa_mulan@163.com>
2025-11-09 14:37:40 +0800

    控制Pastor动画

--- src/pastor.gd
+++ src/pastor.gd
@@ -2,3 +2,6 @@ extends Actor
 
 func _ready() -> void:
 	$animation_tree.get("parameters/playback").start("battle_idle")
+
+func play_animation(anim:String) -> void:
+	$animation_tree.get("parameters/playback").start(anim)
