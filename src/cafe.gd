8780bdaeddce132db5a4ca5c87d69a4b36dff2fd
FaMuLan <fa_mulan@163.com>
2025-11-10 12:30:29 +0800

    其实互动是可以选择性的屏蔽输入

--- src/cafe.gd
+++ src/cafe.gd
@@ -2,10 +2,18 @@ extends Node3D
 
 func _ready() -> void:
 	$player.move_camera($level/camera)
+	$level/table_0/chessboard_standard.set_enabled(false)
 	$level/chessboard/pieces/pastor.play_animation("thinking")
 	$level.interact_list[0x65] = interact_pastor
 
 func interact_pastor() -> void:
 	$player.force_set_camera($level/camera_3d)
+	$level/chessboard.set_enabled(false)
+	$level/table_0/chessboard_standard.set_enabled(true)
+	$level/chessboard/pieces/cheshire.set_position($level/chessboard.convert_name_to_position("e2"))
+	$level/chessboard/pieces/cheshire.play_animation("thinking")
 	await get_tree().create_timer(3).timeout
 	$player.force_set_camera($level/camera)
+	$level/chessboard/pieces/cheshire.play_animation("battle_idle")
+	$level/chessboard.set_enabled(true)
+	$level/table_0/chessboard_standard.set_enabled(false)
