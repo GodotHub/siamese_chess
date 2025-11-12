c87a7a88cbe831112968a5da9e6c853695bb8144
FaMuLan <fa_mulan@163.com>
2025-11-12 17:00:11 +0800

    调整棋子的颜色

--- src/piece.gd
+++ src/piece.gd
@@ -2,7 +2,6 @@ extends Actor
 
 var position_name:String = ""
 var sfx:AudioStreamPlayer3D = null
-var group:int = 0
 var show_on_backup:bool = true
 var backup_position:Vector3 = Vector3(0, 0, 0)
 
@@ -10,7 +9,6 @@ func _ready() -> void:
 	super._ready()
 	top_level = true
 	visible = show_on_backup
-	group = Chess.group(piece_type)
 	var audio_stream_randomizer:AudioStreamRandomizer = AudioStreamRandomizer.new()
 	audio_stream_randomizer.random_pitch = 1.3
 	audio_stream_randomizer.random_volume_offset_db = 2.0
@@ -25,26 +23,6 @@ func _ready() -> void:
 	#else:
 	#	position_name = chessboard.get_position_name(position)
 	#	position = chessboard.convert_name_to_position(position_name)
-		
-	var material:ShaderMaterial = ShaderMaterial.new()
-	material.shader = load("res://src/unmoving_plaid.gdshader")
-	material.set_shader_parameter("texture_albedo", load("res://assets/texture/Texturelabs_Stone_131S.jpg"))
-	material.set_shader_parameter("saturation_mult", 0.85)
-	material.set_shader_parameter("value_mult", 0.728)
-	material.set_shader_parameter("contrast_mult", 0.199)
-	var next_pass_material:StandardMaterial3D = StandardMaterial3D.new()
-	next_pass_material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	next_pass_material.cull_mode = BaseMaterial3D.CULL_FRONT
-	next_pass_material.grow = true
-	next_pass_material.grow_amount = 0.005
-	if group == 1:
-		material.set_shader_parameter("brightness_add", 0.000)
-		next_pass_material.albedo_color = Color(1, 1, 1, 1)
-	else:
-		material.set_shader_parameter("brightness_add", 0.676)
-		next_pass_material.albedo_color = Color(0, 0, 0, 1)
-	material.next_pass = next_pass_material
-	$piece.set_surface_override_material(0, material)
 
 func introduce(_pos:Vector3) -> void:	# 登场动画
 	visible = true
@@ -89,15 +67,6 @@ func promote(_pos:Vector3, _piece:int) -> void:
 		110:
 			instance = load("res://scene/piece_knight_black.tscn").instantiate()
 	add_child(instance)
-
-
-func set_warning(enabled:bool) -> void:
-	if enabled:
-		$piece.get_surface_override_material(0).next_pass.albedo_color = Color(1, 0, 0, 1)
-	elif group == 1:
-		$piece.get_surface_override_material(0).next_pass.albedo_color = Color(1, 1, 1, 1)
-	else:
-		$piece.get_surface_override_material(0).next_pass.albedo_color = Color(0, 0, 0, 1)
 
 func set_show_on_backup(_show_on_backup:bool) -> Actor:
 	show_on_backup = _show_on_backup
