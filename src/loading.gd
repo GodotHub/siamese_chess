a495bcb60a5256666c91c190b94b3a56e00ae799
FaMuLan <fa_mulan@163.com>
2025-11-13 15:42:31 +0800

    黑屏过渡

--- src/loading.gd
+++ src/loading.gd
@@ -4,8 +4,12 @@ var current:Node = null
 
 func _ready() -> void:
 	current = get_tree().current_scene
+	$texture_rect.modulate = Color(1, 1, 1, 0)
 
 func change_scene(path:String, meta:Dictionary) -> void:
+	var tween:Tween = create_tween()
+	tween.tween_property($texture_rect, "modulate", Color(1, 1, 1, 1), 0.3)
+	await tween.finished
 	var instance:Node = load(path).instantiate()
 	for key:String in meta:
 		instance.set_meta(key, meta[key])
@@ -13,3 +17,6 @@ func change_scene(path:String, meta:Dictionary) -> void:
 		current.queue_free()
 	get_tree().root.add_child(instance)
 	current = instance
+	tween.kill()
+	tween = create_tween()
+	tween.tween_property($texture_rect, "modulate", Color(1, 1, 1, 0), 0.3)
