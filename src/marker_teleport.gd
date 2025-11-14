40a66a0a06b3efd57fa270ff40d405be8152e392
FaMuLan <fa_mulan@163.com>
2025-11-14 16:30:00 +0800

    更改传送点为z

--- src/marker_teleport.gd
+++ src/marker_teleport.gd
@@ -1,13 +1,17 @@
-extends Path3D
+extends MarkerBit
 class_name MarkerTeleport
 
-@export var from_level:Level = null
-@export var to_level:Level = null
+@export var to:String = ""
+@export var args:Dictionary = {}
 
-func _ready() -> void:
-	var from:int = Chess.to_position_int(from_level.chessboard.get_position_name(global_position))
-	var to:int = Chess.to_position_int(to_level.chessboard.get_position_name(to_global(curve.get_point_position(curve.get_point_count() - 1))))
-	from_level.teleport.get_or_add(from, {"to": 0, "level": null})["to"] = to
-	from_level.teleport.get_or_add(from, {"to": 0, "level": null})["level"] = to_level
-	to_level.teleport.get_or_add(to, {"to": 0, "level": null})["to"] = from
-	to_level.teleport.get_or_add(to, {"to": 0, "level": null})["level"] = from_level
+func _init() -> void:
+	piece = "z".unicode_at(0)
+
+func change_scene() -> void:
+	var level:Level = get_parent()
+	var by:int = Chess.to_position_int(level.chessboard.get_position_name(position))
+	var from:int = level.chessboard.state.bit_index("k".unicode_at(0))[0]
+	from = Chess.to_x88(from)
+	level.chessboard.execute_move(Chess.create(from, by, 0))
+	await level.chessboard.animation_finished
+	Loading.change_scene(to, args)
