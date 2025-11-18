0c8c10b0747b1831ce0462300cb67a5b93878dca
FaMuLan <fa_mulan@163.com>
2025-11-18 17:02:28 +0800

    在棋局中添加升变选项

--- src/cafe.gd
+++ src/cafe.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	$level/chessboard.add_piece_instance(cheshire_instance, cheshire_by)
 	
 	engine.connect("search_finished", in_game_black)
-	$level/table_0/chessboard_standard.connect("clicked_move", in_game_white)
+	$level/table_0/chessboard_standard.connect("clicked_move", in_game_black_check_move)
 
 	engine.set_max_depth(6)
 	engine.set_think_time(INF)
@@ -66,6 +66,40 @@ func in_game_black() -> void:
 		game_end.call_deferred()
 		return
 	$level/table_0/chessboard_standard.set_valid_move(RuleStandard.generate_valid_move($level/table_0/chessboard_standard.state, 1))
+
+func in_game_black_check_move() -> void:
+	var chessboard:Chessboard = $level/table_0/chessboard_standard
+	var from:int = Chess.from(chessboard.confirm_move)
+	var to:int = Chess.to(chessboard.confirm_move)
+	var valid_move:Dictionary = chessboard.valid_move
+	if from & 0x88 || to & 0x88 || !valid_move.has(from):
+		in_game_black.call_deferred()
+		return
+	var move_list:PackedInt32Array = valid_move[from].filter(func (move:int) -> bool: return to == Chess.to(move))
+	if move_list.size() == 0:
+		in_game_black.call_deferred()
+		return
+	elif move_list.size() > 1:
+		in_game_black_extra_move.call_deferred(move_list)
+	else:
+		in_game_white.call_deferred()
+
+func in_game_black_extra_move(move_list:PackedInt32Array) -> void:
+	Dialog.clear()
+	Dialog.disconnect("on_next", on_select_dialog)
+	var decision_list:PackedStringArray = []
+	var decision_to_move:Dictionary = {}
+	for iter:int in move_list:
+		decision_list.push_back("%c" % Chess.extra(iter))
+		decision_to_move[decision_list[-1]] = iter
+	decision_list.push_back("cancel")
+	Dialog.connect("on_next", func () -> void:
+		if Dialog.selected == "cancel":
+			in_game_black.call_deferred()
+		else:
+			in_game_white.call_deferred()
+	)
+	Dialog.push_selection(decision_list, true, true)
 
 func on_select_dialog() -> void:
 	if Dialog.selected == "悔棋":
