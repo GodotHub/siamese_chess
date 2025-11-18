35bc81b2a63a2c97e35f973e32cb563a30280d8f
FaMuLan <fa_mulan@163.com>
2025-11-18 16:47:54 +0800

    允许直接退出棋局

--- src/cafe.gd
+++ src/cafe.gd
@@ -29,8 +29,9 @@ func _ready() -> void:
 func interact_pastor() -> void:
 	var from:int = $level/chessboard.state.bit_index("k".unicode_at(0))[0]
 	from = Chess.to_x88(from)
-	$level/chessboard.execute_move(Chess.create(from, 0x54, 0))
-	await $level/chessboard.animation_finished
+	if from != 0x54:
+		$level/chessboard.execute_move(Chess.create(from, 0x54, 0))
+		await $level/chessboard.animation_finished
 	$level/chessboard.set_enabled(false)
 	$level/table_0/chessboard_standard.set_enabled(true)
 	$level/chessboard/pieces/cheshire.set_position($level/chessboard.convert_name_to_position("e2"))
@@ -45,6 +46,8 @@ func in_game() -> void:
 	in_game_white.call_deferred()
 
 func in_game_white() -> void:
+	Dialog.clear()
+	Dialog.disconnect("on_next", on_select_dialog)
 	if $level/table_0/chessboard_standard.confirm_move != 0:
 		history_state.push_back($level/table_0/chessboard_standard.state.get_zobrist())
 		$level/table_0/chessboard_standard.execute_move($level/table_0/chessboard_standard.confirm_move)
@@ -55,12 +58,20 @@ func in_game_white() -> void:
 	engine.start_search($level/table_0/chessboard_standard.state, 0, history_state, Callable())
 
 func in_game_black() -> void:
+	Dialog.push_selection(["悔棋", "离开对局"], false, false)
+	Dialog.connect("on_next", on_select_dialog, ConnectFlags.CONNECT_ONE_SHOT)
 	history_state.push_back($level/table_0/chessboard_standard.state.get_zobrist())
 	$level/table_0/chessboard_standard.execute_move(engine.get_search_result())
 	if RuleStandard.get_end_type($level/table_0/chessboard_standard.state) != "":
 		game_end.call_deferred()
 		return
 	$level/table_0/chessboard_standard.set_valid_move(RuleStandard.generate_valid_move($level/table_0/chessboard_standard.state, 1))
+
+func on_select_dialog() -> void:
+	if Dialog.selected == "悔棋":
+		pass
+	elif Dialog.selected == "离开对局":
+		game_end.call_deferred()
 
 func game_end() -> void:
 	$player.force_set_camera($level/camera)
