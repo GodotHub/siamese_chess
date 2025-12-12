a43488e85bdc74172c2979ab022c9657df5af02f
FaMuLan <fa_mulan@163.com>
2025-12-12 20:31:46 +0800

    二楼三楼的电梯门口也可以自选楼层了

--- src/level/hallway_3f.gd
+++ src/level/hallway_3f.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	title[0x02] = "电梯"
 	interact_list[0x02] = {
 		"1F": Loading.change_scene.bind("res://scene/level/reception_lobby.tscn", {"by": 2}),
-		"3F": Loading.change_scene.bind("res://scene/level/hallway_3f.tscn", {"by": 2})}
+		"2F": Loading.change_scene.bind("res://scene/level/hallway_2f.tscn", {"by": 2})}
 	$chessboard.state.add_piece(cheshire_by, "k".unicode_at(0))
 	$chessboard.add_piece_instance(cheshire_instance, cheshire_by)
 	$player.force_set_camera($camera)
