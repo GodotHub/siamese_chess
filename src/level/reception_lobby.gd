38a19061adcc490784fcabffe970295060e6774c
FaMuLan <fa_mulan@163.com>
2025-12-12 16:47:28 +0800

    可以自行选择前往哪个楼层了

--- src/level/reception_lobby.gd
+++ src/level/reception_lobby.gd
@@ -5,6 +5,10 @@ func _ready() -> void:
 	var cheshire_by:int = get_meta("by")
 	var cheshire_instance:Actor = load("res://scene/actor/cheshire.tscn").instantiate()
 	cheshire_instance.position = $chessboard.convert_name_to_position(Chess.to_position_name(cheshire_by))
+	title[0x02] = "电梯"
+	interact_list[0x02] = {
+		"2F": Loading.change_scene.bind("res://scene/level/hallway_2f.tscn", {"by": 2}),
+		"3F": Loading.change_scene.bind("res://scene/level/hallway_3f.tscn", {"by": 2})}
 	$chessboard.state.add_piece(cheshire_by, "k".unicode_at(0))
 	$chessboard.add_piece_instance(cheshire_instance, cheshire_by)
 	$player.force_set_camera($camera)
