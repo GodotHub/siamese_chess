7dcd675e1521ac868a64a39da1d1a4b1366fc730
FaMuLan <fa_mulan@163.com>
2025-12-18 16:08:51 +0800

    加了一堆画框，旋转一下咖啡厅的布局

--- src/level/reception_lobby.gd
+++ src/level/reception_lobby.gd
@@ -5,8 +5,8 @@ func _ready() -> void:
 	var cheshire_by:int = get_meta("by")
 	var cheshire_instance:Actor = load("res://scene/actor/cheshire.tscn").instantiate()
 	cheshire_instance.position = $chessboard.convert_name_to_position(Chess.to_position_name(cheshire_by))
-	title[0x02] = "电梯"
-	interact_list[0x02] = {
+	title[0x01] = "电梯"
+	interact_list[0x01] = {
 		"2F": Loading.change_scene.bind("res://scene/level/hallway_2f.tscn", {"by": 2}),
 		"3F": Loading.change_scene.bind("res://scene/level/hallway_3f.tscn", {"by": 2})}
 	$chessboard.state.add_piece(cheshire_by, "k".unicode_at(0))
