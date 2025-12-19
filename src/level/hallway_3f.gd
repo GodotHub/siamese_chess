f3ac4f20bf3e1573a4bb934e8cceed3eeaeed131
FaMuLan <fa_mulan@163.com>
2025-12-19 15:32:43 +0800

    加了窗帘，以及一张矮桌子

--- src/level/hallway_3f.gd
+++ src/level/hallway_3f.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	cheshire_instance.position = $chessboard.convert_name_to_position(Chess.to_position_name(cheshire_by))
 	title[0x02] = "电梯"
 	interact_list[0x02] = {
-		"1F": Loading.change_scene.bind("res://scene/level/reception_lobby.tscn", {"by": 2}),
+		"1F": Loading.change_scene.bind("res://scene/level/reception_lobby.tscn", {"by": 1}),
 		"2F": Loading.change_scene.bind("res://scene/level/hallway_2f.tscn", {"by": 2})}
 	$chessboard.state.add_piece(cheshire_by, "k".unicode_at(0))
 	$chessboard.add_piece_instance(cheshire_instance, cheshire_by)
