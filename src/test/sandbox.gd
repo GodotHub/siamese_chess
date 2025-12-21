263c1031609e56c4226c59aa1522d7c86c2d4076
FaMuLan <fa_mulan@163.com>
2025-12-22 01:49:02 +0800

    恢复沙盒的功能

--- src/test/sandbox.gd
+++ src/test/sandbox.gd
@@ -6,7 +6,6 @@ var initial_state:State = null
 
 func _ready() -> void:
 	$player.force_set_camera($camera_3d)
-	chessboard.connect("clicked_move", receive_move)
 	chessboard.set_enabled(true)
 	while !is_instance_valid(state):
 		var text_input_instance:TextInput = TextInput.create_text_input_instance("输入FEN格式的布局：", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
@@ -16,38 +15,46 @@ func _ready() -> void:
 	initial_state = state.duplicate()
 	chessboard.set_state(state.duplicate())
 	chessboard.add_default_piece_set()
-	update_move()
+	select_first()
 
 func _unhandled_input(event:InputEvent) -> void:
 	if event is InputEventKey && event.is_pressed() && event.keycode == KEY_R:
 		reset()
 
-func receive_move() -> void:
-	await check_move()
-	chessboard.execute_move(chessboard.confirm_move)
-	Chess.apply_move(state, chessboard.confirm_move)
-	update_move()
+func select_first() -> void:
+	chessboard.set_square_selection(
+		state.get_bit(ord("K")) | state.get_bit(ord("k")) |
+		state.get_bit(ord("Q")) | state.get_bit(ord("q")) |
+		state.get_bit(ord("R")) | state.get_bit(ord("r")) |
+		state.get_bit(ord("B")) | state.get_bit(ord("b")) |
+		state.get_bit(ord("N")) | state.get_bit(ord("n")) |
+		state.get_bit(ord("P")) | state.get_bit(ord("p"))
+	)
+	await chessboard.click_selection
+	select_second.call_deferred()
 
-func update_move() -> void:
+func select_second() -> void:
+	var from:int = chessboard.selected
 	var move_list:PackedInt32Array = Chess.generate_valid_move(state, state.get_turn())
-	chessboard.set_valid_move(move_list)
-
-func check_move() -> bool:
-	var from:int = Chess.from(chessboard.confirm_move)
-	var to:int = Chess.to(chessboard.confirm_move)
-	var valid_move:Dictionary = chessboard.valid_move
-	if from & 0x88 || to & 0x88 || !valid_move.has(from):
-		return false
-	var move_list:PackedInt32Array = valid_move[from].filter(func (move:int) -> bool: return to == Chess.to(move))
-	if move_list.size() == 0:
-		return false
-	elif move_list.size() > 1:
-		return await select_move(move_list)
+	var selection:int = 0
+	for iter:int in move_list:
+		if Chess.from(iter) == from:
+			selection |= Chess.mask(Chess.to_64(Chess.to(iter)))
+	chessboard.set_square_selection(selection)
+	await chessboard.clicked
+	if Chess.mask(Chess.to_64(chessboard.selected)) & selection:
+		check_move.call_deferred(from, chessboard.selected, move_list)
 	else:
-		chessboard.confirm_move = move_list[0]
-		return true
+		select_first.call_deferred()
 
-func select_move(move_list:PackedInt32Array) -> bool:
+func check_move(from:int, to:int, move_list:PackedInt32Array) -> void:
+	move_list = Array(move_list).filter(func (move:int) -> bool: return from == Chess.from(move) && to == Chess.to(move))
+	if move_list.size() > 1:
+		select_move.call_deferred(move_list)
+	elif move_list.size() == 1:
+		apply_move.call_deferred(move_list[0])
+
+func select_move(move_list:PackedInt32Array) -> void:
 	var decision_list:PackedStringArray = []
 	var decision_to_move:Dictionary = {}
 	for iter:int in move_list:
@@ -57,12 +64,15 @@ func select_move(move_list:PackedInt32Array) -> bool:
 	Dialog.push_selection(decision_list, "请选择一个着法", true, true)
 	await Dialog.on_next
 	if Dialog.selected == "cancel":
-		return false
+		select_first.call_deferred()
 	else:
-		chessboard.confirm_move = decision_to_move[Dialog.selected]
-		return true
+		apply_move.call_deferred(decision_to_move[Dialog.selected])
+
+func apply_move(move:int) -> void:
+	chessboard.execute_move(move)
+	Chess.apply_move(state, move)
+	select_first.call_deferred()
 
 func reset() -> void:
 	state = initial_state.duplicate()
 	chessboard.set_state(initial_state)
-	update_move()
