d0138ba53c3400ee0cb6fbbb03b82f59a1231788
FaMuLan <fa_mulan@163.com>
2025-12-31 21:38:37 +0800

    将自动传送更改为手动传送

--- src/marker_teleport.gd
+++ src/marker_teleport.gd
@@ -1,8 +1,11 @@
-extends MarkerEvent
+extends MarkerSelection
 class_name MarkerTeleport
 
 @export var to:String = ""
 @export var args:Dictionary = {}
+
+func _init() -> void:
+	selection = "前往"
 
 func event() -> void:
 	var from:int = Chess.to_position_int(level.chessboard.get_position_name(position))
