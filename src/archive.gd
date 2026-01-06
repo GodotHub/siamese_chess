5ea3c9769360715f88a11f6a087a5328eac01e6c
FaMuLan <fa_mulan@163.com>
2026-01-06 17:03:21 +0800

    更改文档类型，这类文档通常一张图即全部内容

--- src/archive.gd
+++ src/archive.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 var template_list:Dictionary = {
-	"menu": "res://scene/menu.tscn",
+	"printed": "res://scene/printed_paper.tscn",
 	"history": "res://scene/history.tscn",
 	"draft": "res://scene/draft.tscn",
 	"piece": "res://scene/model.tscn",
---
+++ src/printed_paper.gd
@@ -0,0 +1,18 @@
+extends Document
+
+@export var img_path:String = ""
+
+func _ready() -> void:
+	$sprite_2d.texture = load(img_path)
+
+func parse(data:String) -> void:
+	var data_dict:Dictionary = JSON.parse_string(data)
+	img_path = data_dict["path"]
+	$sprite_2d.texture = load(img_path)
+	draw_lines(data_dict["lines"])
+
+func stringify() -> String:
+	var data_dict:Dictionary = {}
+	data_dict["path"] = img_path
+	data_dict["lines"] = get_lines()
+	return JSON.stringify(data_dict)
